497c7c64681ec24ec76d7e73c78a7f00395e71fb
Denis Laliberté <denis@tp1.ca>
2015-01-25 21:24:07 -0500

    refactor test to use before hook

--- QueueKata.rb/queue_spec.rb
+++ QueueKata.rb/queue_spec.rb
@@ -1,18 +1,18 @@
 require './queue.rb'
 
 describe "queue" do
+  before(:each) do
+    @queue = Queue.new()
+  end
   it "new queue is empty" do
-    queue = Queue.new()
-    expect(queue.empty()).to eq(true)
+    expect(@queue.empty()).to eq(true)
   end
   it "is not empty when an item is push" do
-    queue = Queue.new()
-    queue.push(1)
-    expect(queue.empty()).to eq(false)
+    @queue.push(1)
+    expect(@queue.empty()).to eq(false)
   end
   it "return an item" do
-    queue = Queue.new()
-    queue.push(1)
-    expect(queue.pop).to eq(1)
+    @queue.push(1)
+    expect(@queue.pop).to eq(1)
   end
 end
