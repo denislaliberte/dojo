e3c0daeab19d736ea15758f1ba10aa3d46d230e0
Denis Laliberté <denis@tp1.ca>
2015-09-02 12:38:58 -0400

    php-fb3 fizzbuzz end

--- FizzBuzz/php-fb3--fizzbuzz-third-iteration/Fizzbuzz.php
+++ FizzBuzz/php-fb3--fizzbuzz-third-iteration/Fizzbuzz.php
@@ -2,9 +2,10 @@
 
 function fizzbuzz(array $input) {
   $fizzbuzz = function($item) {
-    $return = $item;
-    if($item % 3 == 0) $return ='fizz';
-    if($item % 5 == 0) $return ='buzz';
+    $return = '';
+    if($item % 3 == 0) $return .= 'fizz';
+    if($item % 5 == 0) $return .= 'buzz';
+    if(empty($return)) $return = $item;
     return $return;
   };
   return array_map($fizzbuzz, $input);
--- FizzBuzz/php-fb3--fizzbuzz-third-iteration/FizzbuzzTest.php
+++ FizzBuzz/php-fb3--fizzbuzz-third-iteration/FizzbuzzTest.php
@@ -1,7 +1,6 @@
 <?php
 class aTest extends PHPUnit_Framework_TestCase { 
   function test_acceptance() {
-    $this->markTestIncomplete('acceptance test not complete');
     $this->assertEquals(fizzbuzz(range(1,15)), [1,2,'fizz',4,'buzz','fizz',7,8,'fizz','buzz',11,'fizz',13,14,'fizzbuzz']);
   }
 
@@ -15,5 +14,9 @@ class aTest extends PHPUnit_Framework_TestCase {
 
   function test_buzz() {
     $this->assertEquals(fizzbuzz([5]), ['buzz']);
+  }
+
+  function test_fizzbuzz() {
+    $this->assertEquals(fizzbuzz([15]), ['fizzbuzz']);
   }
 }
