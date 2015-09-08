f2036cb4b4768fed60444ddc6a3aa0daf0ccedc4
Denis Laliberté <denis@tp1.ca>
2015-09-08 18:34:24 -0400

    php-1 add 900 and 400 case

--- RomanNumber/php-1-roman-number/RomanNumber.php
+++ RomanNumber/php-1-roman-number/RomanNumber.php
@@ -1,17 +1,21 @@
 <?php
 
+require('/Users/TP1/.composer/vendor/bin/psysh');
+
 function romannumber($input, $a = [[1000,'M'],[500,'D'],[100,'C'],[50,'L'],[10,'X'],[5,'V',],[1,'I']]) {
   if(empty($a)) { return ""; }
   if($input > head($a)[0]) { return  head($a)[1] . romannumber($input - head($a)[0], $a); }
   if(head($a)[0] == $input) { return head($a)[1] . romannumber($input - head($a)[0], tail($a));  }
-  return romannumber($input,tail($a));
+  if($input == head($a)[0] - next_ten_multiple($a)[0]) { 
+    return next_ten_multiple($a)[1] .  head($a)[1] 
+      . romannumber($input - (head($a)[0] - next_ten_multiple($a)[1]), tail($a)); }
+  return romannumber($input, tail($a));
 }
 
-function head(array $array, $default = null) {
-  return isset($array[0]) ? $array[0] : $defaul;
+function next_ten_multiple(array $a) {
+  return [100,'C'];
 }
 
-function tail(array $array) {
-  array_shift($array);
-  return $array;
-}  
+function head(array $array, $default = null) { return isset($array[0]) ? $array[0] : $defaul;}
+
+function tail(array $array) { array_shift($array); return $array; }  
--- RomanNumber/php-1-roman-number/RomanNumberTest.php
+++ RomanNumber/php-1-roman-number/RomanNumberTest.php
@@ -20,5 +20,9 @@ class RomanNumberTest extends PHPUnit_Framework_TestCase {
     $this->assertEquals(romannumber(30), 'XXX');
   }
 
+  public function test_special_case() {
+    $this->assertEquals(romannumber(900), 'CM');
+    $this->assertEquals(romannumber(400), 'CD');
 
+  }
 }
